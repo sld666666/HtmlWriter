8fb61a51162c1a894e4d75946c64a8810e6b8179
sld666666 <sld666666@gmai.com>
2013-01-27 22:05:07 +0800

    and markdown parser

--- HtmlWriter/htmlwriter.cpp
+++ HtmlWriter/htmlwriter.cpp
@@ -1,12 +1,175 @@
+#include <QDir>
+#include <QFile>
+#include <QTextStream>
+#include <fstream>
+
 #include "htmlwriter.h"
+#include "FileCreator.h"
+#include "markdown/markdown-tokens.h"
 
 HtmlWriter::HtmlWriter(QWidget *parent, Qt::WFlags flags)
 	: QMainWindow(parent, flags)
 {
 	ui.setupUi(this);
+	initFileInfoCtrl();
+	innitConnect();
 }
 
 HtmlWriter::~HtmlWriter()
 {
 
+}
+
+void HtmlWriter::initFileInfoCtrl()
+{
+	QString test = QDir::currentPath();
+	model_.setRootPath(QDir::currentPath());
+	ui.treeViewFileInfo->setRootIndex(model_.index("E:\\project"));
+	ui.treeViewFileInfo->setModel(&model_);
+	ui.treeViewFileInfo->setAnimated(false);
+	ui.treeViewFileInfo->setIndentation(20);
+	ui.treeViewFileInfo->setSortingEnabled(true);
+	ui.treeViewFileInfo->setContextMenuPolicy(Qt::CustomContextMenu);
+}
+
+void HtmlWriter::innitConnect()
+{
+	connect(ui.treeViewFileInfo, SIGNAL(customContextMenuRequested(const QPoint))
+		, this, SLOT(showRightMenuSlot(const QPoint)));
+
+	connect(ui.treeViewFileInfo, SIGNAL(doubleClicked(const QModelIndex &))
+		, this, SLOT(onDoubleClickedItemSlot(const QModelIndex &)));
+
+	connect(ui.actionSave, SIGNAL(triggered()), this, SLOT(onSaveSlot()));
+}
+
+void HtmlWriter::showRightMenuSlot(const QPoint point)
+{
+	QMenu *qMenu = NULL;
+
+	qMenu = new QMenu(ui.treeViewFileInfo);
+
+	QAction* acttionNewFolder = new QAction("&new folder",this);
+	connect(acttionNewFolder, SIGNAL(triggered()), this, SLOT(newFolderSlot()));
+
+	QAction* actionNewFile = new QAction("&new file", this);
+	connect(actionNewFile, SIGNAL(triggered()), this, SLOT(newFileSlot()));
+
+	QAction* actionDelete = new QAction("&delete", this);
+	connect(actionDelete, SIGNAL(triggered()), this, SLOT(deleteSlot()));
+
+	qMenu->addAction(acttionNewFolder);
+	qMenu->addAction(actionNewFile);
+
+	qMenu->exec(QCursor::pos()); //在鼠标点击的位置显示鼠标右键菜单
+}
+
+void HtmlWriter::newFolderSlot()
+{
+	QString newFilePath = getSelectItemPath();
+	if ((!newFilePath.isEmpty()) 
+		&& getNewFilePath(newFilePath, newFilePath)){
+		if (!QDir().mkdir(newFilePath)){
+		}	
+	}	
+}
+
+QString HtmlWriter::getSelectItemPath()
+{
+	QString itemPath("");
+	QItemSelectionModel* selecitonModel = ui.treeViewFileInfo->selectionModel();
+	if (selecitonModel){
+		QModelIndex modelIndex = selecitonModel->currentIndex();
+		itemPath = getFullPath(modelIndex); 
+	}
+	return itemPath;
+}
+
+void HtmlWriter::newFileSlot()
+{
+	QString newFilePath = getSelectItemPath();
+	if ((!newFilePath.isEmpty()) 
+		&& getNewFilePath(newFilePath, newFilePath)){
+			QFile file(newFilePath);
+			if(file.open(QFile::ReadWrite|QFile::Text)){
+				file.close();  
+			}
+	}	
+}
+
+bool HtmlWriter::getNewFilePath(const QString& itemPath
+								, QString& rtnPath)
+{
+	bool rtn(false);
+	rtnPath = itemPath;
+	FileCreator fileCreator(rtnPath, this);
+	fileCreator.show();
+	if (QDialog::Accepted == fileCreator.exec()){
+		rtnPath = fileCreator.filePath();
+		rtn = true;
+	}
+
+	return rtn;
+}
+
+QString HtmlWriter::getFullPath(const QModelIndex& modelIndx)
+{
+	Q_ASSERT(modelIndx.isValid());
+
+	QString path("");
+
+	const QAbstractItemModel* model = modelIndx.model();
+	if (model){
+		path = model->data(modelIndx).toString();
+		QModelIndex parentIndex = modelIndx.parent();
+		if (parentIndex.isValid()){
+			path = QDir::separator() + path;
+			path = getFullPath(parentIndex)+path;
+		}
+	}
+	return path;
+}
+
+void HtmlWriter::deleteSlot()
+{
+
+}
+
+void HtmlWriter::onDoubleClickedItemSlot(const QModelIndex & modelIndex)
+{
+	QString itemPath = getFullPath(modelIndex); 
+	workingItemPath_ = itemPath;
+	QFile file(itemPath); // Read the text from a file
+	if (file.open(QIODevice::ReadOnly)) {
+		QTextStream stream(&file);
+		ui.textEditText_->setText(stream.readAll());
+	}
+}
+
+void HtmlWriter::onSaveSlot()
+{
+	if (!workingItemPath_.isEmpty()){
+		QString text = ui.textEditText_->toPlainText();
+		QFile file(workingItemPath_); 
+		if (file.open(QIODevice::WriteOnly)) {
+			QTextStream stream(&file);
+			stream<<text;
+		}
+	}
+}
+
+QString HtmlWriter::makeDownToHtml(const QString& input)
+{
+	markdown::Document doc;
+	doc.read(input.toStdString());
+	std::ofstream  out;
+	doc.write(out);
+	
+	//std string htmlString;
+	//copy(htmlString.begin(),htmlString.end(),std::ostream_iterator<char>(cout,"\n"));这里改了
+	long  size=out.tellg();
+	char * buffer = new char [size];
+	out.write (buffer,size);
+
+	return "";
 }
--- HtmlWriter/htmlwriter.h
+++ HtmlWriter/htmlwriter.h
@@ -1,6 +1,14 @@
+/** 
+* @file         htmlwriter.h 
+* @Synopsis       
+* @author         sld666666@gmail.com 
+* @version     1.0.0
+* @date         27:1:2013  
+*/
 #ifndef HTMLWRITER_H
 #define HTMLWRITER_H
 
+#include <QFileSystemModel>
 #include <QtGui/QMainWindow>
 #include "ui_htmlwriter.h"
 
@@ -12,8 +20,25 @@ public:
 	HtmlWriter(QWidget *parent = 0, Qt::WFlags flags = 0);
 	~HtmlWriter();
 
+private slots:
+	void	showRightMenuSlot(const QPoint point);
+	void	newFolderSlot();
+	void	newFileSlot();
+	void	deleteSlot();
+	void	onDoubleClickedItemSlot(const QModelIndex & modelIndex);
+	void	onSaveSlot();
+
+private:
+	void	initFileInfoCtrl();
+	void	innitConnect();
+	QString getFullPath(const QModelIndex& modelIndx);
+	bool	getNewFilePath(const QString& itemPath, QString& rtnPath);
+	QString getSelectItemPath();
+	QString makeDownToHtml(const QString& input);
 private:
 	Ui::HtmlWriterClass ui;
+	QFileSystemModel model_;
+	QString	workingItemPath_;
 };
 
 #endif // HTMLWRITER_H
