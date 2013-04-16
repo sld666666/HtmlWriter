22c801f8d561c892b8c75b7866a56dc9b036468c
sld666666 <sld666666@gmail.com>
2013-04-16 21:27:22 +0800

    add IContext.h

---
+++ Ui/context/IContext.h
@@ -0,0 +1,26 @@
+/** 
+* @file         IContext.h 
+* @Synopsis       
+* @author         diwu.sld
+* @version      1.0
+* @date         2013-4-16
+*/
+#ifndef UIUTILS_UiUtils_H
+#define UIUTILS_UiUtils_H
+
+#include "ui_global.h"
+
+namespace UiUtils{
+
+	class UI_EXPORT IContext 
+	{
+		
+	public:
+		IContext(){}
+		~IContext(){}
+	};
+
+}
+
+
+#endif // UI_UiUtils_H
